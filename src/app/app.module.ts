5dce69a3ce7a614daeb767a3a61444c23f7c9b65
parasmalhotra522@gmail.com <parasmalhotra522@gmail.com>
2023-09-10 18:49:07 -0400

    Home Page of the Portfolio done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatToolbarModule,
+    MatButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  imgUrl = "../../assets/myPhoto.jpeg";
+  recommendations : any = [
+    "../../assets/shalem-review.png",
+    "../../assets/gokul-review.png",
+    "../../assets/vishwa-review.png",
+    "../../assets/roopesh-review.png"
+    
+
+  ];
+ 
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+ 
+  }
+
+}
